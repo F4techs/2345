2de2549a828885838f179318ea0597e23b2d30c6
F4techs <86817323+F4techs@users.noreply.github.com>
2021-09-05 18:04:45 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -30,7 +30,7 @@ async def start(bot, update):
             await bot.send_document(
                 chat_id=update.chat.id,
                 document = file_id,
-                caption = support  @GKCCU098,
+                caption = @GKCCU098,
                 parse_mode="html",
                 reply_to_message_id=update.message_id,
                 reply_markup=InlineKeyboardMarkup(
